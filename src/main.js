3b43e891e6c3922f5813c63c9e5bab7a139cc870
LightDiscord <arnaud@lightdiscord.me>
2018-07-08 20:10:21 +0200

    Filter and new line

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import cli from 'command-line-args'
 import stdin from 'get-stdin'
-import { TYPES, gather, commentRegExp } from '../lib/lines'
+import { TYPES, gather, commentRegExp, isValid } from '../lib/lines'
 
 const { language = "", comment } = cli([
     { name: 'comment', alias: 'c', type: String },
@@ -11,11 +11,11 @@ if (!comment) throw new Error('Flag comment not defined')
 
 const format = {
     [TYPES.Comment]: lines => lines.map(line => line.trim().replace(commentRegExp(comment), '').trim()),
-    [TYPES.Regular]: lines => `${'```'}${language}\n${lines.join('\n')}${'```'}`
+    [TYPES.Regular]: lines => `${'```'}${language}\n${lines.join('\n')}\n${'```'}`
 }
 
 stdin().then(text => {
-    const groups = gather(text.split('\n'), comment)
+    const groups = gather(text.split('\n').filter(isValid), comment)
     const result = groups.map(group => format[group.type](group.lines))
 
     console.log(result.join`\n`)
